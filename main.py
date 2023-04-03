996c2f6385e75393043f6842889fc2efd7d99008
Mehonal <116497904+mehonal@users.noreply.github.com>
2023-04-03 07:02:55 +0300

    added precaution against null file date

--- main.py
+++ main.py
@@ -30,26 +30,30 @@ else:
             else:
                 file_timestamp = os.path.getctime(file)        
             file_date = datetime.datetime.fromtimestamp(file_timestamp)
-            print(f'Date: {file_date}')
-            folder_path = ""
-            if SETTINGS.YEAR_FOLDER:
-                folder_path += str(file_date.year) + "/"
-            if SETTINGS.MONTH_FOLDER:
-                folder_path += str(file_date.month) + "/"
-            if SETTINGS.DAY_FOLDER:
-                folder_path += str(file_date.day) + "/"
-            if SETTINGS.HOUR_FOLDER:
-                folder_path += str(file_date.hour) + "/"
-            if SETTINGS.MINUTE_FOLDER:
-                folder_path += str(file_date.minute) + "/"
-            if folder_path[-1] == "/":
-                folder_path = folder_path[:-1]
-            print(folder_path)
-            Path(folder_path).mkdir(parents=True, exist_ok=True)
-            if os.path.exists(folder_path):
-                print(f"Moving {file} to {folder_path}")
-                shutil.move(os.getcwd() + "/" + file, folder_path + "/" + file)
-            else:
-                print(f"Missing folder!")
+            if file_date and file_date is not None:
+                print(f'{file}: {file_date}')
+                folder_path = ""
+                if SETTINGS.YEAR_FOLDER:
+                    folder_path += str(file_date.year) + "/"
+                if SETTINGS.MONTH_FOLDER:
+                    folder_path += str(file_date.month) + "/"
+                if SETTINGS.DAY_FOLDER:
+                    folder_path += str(file_date.day) + "/"
+                if SETTINGS.HOUR_FOLDER:
+                    folder_path += str(file_date.hour) + "/"
+                if SETTINGS.MINUTE_FOLDER:
+                    folder_path += str(file_date.minute) + "/"
+                if folder_path[-1] == "/":
+                    folder_path = folder_path[:-1]
+                print(folder_path)
+                Path(folder_path).mkdir(parents=True, exist_ok=True)
+                if folder_path != "":
+                    if os.path.exists(folder_path):
+                        print(f"Moving {file} to {folder_path}")
+                        shutil.move(os.getcwd() + "/" + file, folder_path + "/" + file)
+                    else:
+                        print(f"Missing folder!")
+                else:
+                    print("Skipped file")
 
 print("Script finished executing.")
